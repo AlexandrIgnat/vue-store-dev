59b5ae629b36ca667cfd2e807a45995dfbdda0b6
Игнатьев Александр <ignatyevaleksandr.1992@mail.ru>
2023-07-07 17:03:03 +0300

    first commit

---
+++ src/data/products.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        title: 'Конструктор LEGO Speed Champions 76906 1970 Ferrari 512 M',
+        price: 2474,
+        image: 'https://ir-2.ozone.ru/s3/multimedia-c/wc1000/6398629452.jpg',
+    },
+    {
+        title: 'Конструктор детский LEGO Speed Champions Автомобиль P963, 280 деталей, 9+, 76916',
+        price: 1932,
+        image: 'https://ir-2.ozone.ru/s3/multimedia-x/wc1000/6640658373.jpg',
+    },{
+        title: 'Конструктор LEGO Speed Champions 007 Aston Martin DB5 76911',
+        price: 2990,
+        image: 'https://ir-2.ozone.ru/s3/multimedia-o/wc1000/6431650788.jpg',
+    },{
+        title: 'Конструктор LEGO Speed Champions Fast and Furious 1970 Dodge Charger R/T 76912',
+        price: 3290,
+        image: 'https://ir-2.ozone.ru/s3/multimedia-a/wc1000/6429447934.jpg',
+    },{
+        title: 'Конструктор LEGO Speed Champions Lotus',
+        price: 3337,
+        image: 'https://ir-2.ozone.ru/s3/multimedia-f/wc1000/6265523595.jpg',
+    },{
+        title: 'Конструктор LEGO City Great Vehicles Мобильный кран, 340 деталей, 7+, 60324',
+        price: 3329,
+        image: 'https://ir-2.ozone.ru/s3/multimedia-v/wc1000/6205741507.jpg',
+    },{
+        title: 'Конструктор LEGO City Автомойка, 243 детали, 6+, 60362',
+        price: 2337,
+        image: 'https://ir-2.ozone.ru/s3/multimedia-i/wc1000/6684247482.jpg',
+    },{
+        title: 'Конструктор LEGO City Great Vehicles Мобильный кран, 340 деталей, 7+, 60324Конструктор LEGO City Магазин мороженого, 296 деталей, 6+, 60363',
+        price: 3518,
+        image: 'https://ir-2.ozone.ru/s3/multimedia-s/wc1000/6684236548.jpg',
+    },{
+        title: 'Конструктор детский LEGO Technic Автомобиль Camaro ZL1, 672 деталей, 9+, 42153',
+        price: 4261,
+        image: 'https://ir-2.ozone.ru/s3/multimedia-7/wc1000/6636270055.jpg',
+    }, {
+        title: 'Конструктор детский LEGO Speed Champions Автомобиль P963, 280 деталей, 9+, 76916',
+        price: 1932,
+        image: 'https://ir-2.ozone.ru/s3/multimedia-x/wc1000/6640658373.jpg',
+    },{
+        title: 'Конструктор LEGO City Great Vehicles Мобильный кран, 340 деталей, 7+, 60324',
+        price: 3329,
+        image: 'https://ir-2.ozone.ru/s3/multimedia-v/wc1000/6205741507.jpg',
+    }
+    
+
+]
