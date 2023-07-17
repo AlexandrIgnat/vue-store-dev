040cd652eb44711eedebf52127bb60afa1c2cc85
Игнатьев Александр <ignatyevaleksandr.1992@mail.ru>
2023-07-17 17:32:23 +0300

    231

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,73 @@
 <template>
   <div>
-    <section class="catalog">
-        <Productlist  :products="products" />
-        <BasePagination v-model="page" :count="count" :per-page="productPerPage" />
-      </section>
+
+    <main class="content container">
+      <div class="content__top content__top--catalog">
+        <h1 class="content__title">
+          Каталог
+        </h1>
+        <span class="content__info">
+          152 товара
+        </span>
+      </div>
+
+      <div class="content__catalog">
+
+        <ProductFilter />
+
+        <section class="catalog">
+          <Productlist :products="products" />
+          <BasePagination v-model="page" :count="count" :per-page="productPerPage" />
+        </section>
+
+      </div>
+    </main>
+
   </div>
 </template>
 
 <script>
 
-import  products from './data/products'
+import products from './data/products'
 import Productlist from './components/ProductList.vue';
 import BasePagination from './components/BasePagination.vue';
+import ProductFilter from './components/ProductFilter.vue';
 
 export default {
   name: 'App',
   data() {
     return {
+      filterPriceFrom: 0,
+      filterPriceTo: 0,
+      filterCategoryId: 0,
+
       page: 1,
       productPerPage: 3,
     }
   },
   computed: {
+    filteredProducts() {
+      let filteredProducts = products;
+
+      if (this.filterPriceFrom > 0) {
+        filteredProducts = filteredProducts.filter(product => product.price > this.filterPriceFrom)
+      }
+
+      return filteredProducts;
+    },
     products() {
       const offset = (this.page - 1) * this.productPerPage;
       return products.slice(offset, offset + this.productPerPage);
     },
     count() {
-      return products.length 
+      return products.length
     },
 
   },
   components: {
     Productlist,
     BasePagination,
+    ProductFilter,
   }
 }
 </script>
